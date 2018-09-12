5b4751d326b0f72fdb418d5a9369b1894cb14ba3
Dillon Reinwald <dreinwaldmail@gmail.com>
2018-09-12 14:26:23 -0700

    started functions

--- liri.js
+++ liri.js
@@ -4,3 +4,34 @@ require("dotenv").config();
 //import keys.js to acces sportify api key
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var doThis = process.argv[2];
+
+switch (doThis){
+    case "concert-this":
+        concert();
+        break;
+    case "spotify-this-song":
+        spotify();
+        break;
+    case "movie-this":
+        movie();
+        break;
+    case "do-what-it-says":
+        doWhat();
+        break;
+};
+//concert-this
+var artist = process.argv[3];
+var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+requestAnimationFrame(queryURL, function (error, response, body){
+    
+})
+
+//spotify-this-song
+    
+//movie-this
+
+//do-what-it-says
+
